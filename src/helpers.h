b531cec077ed7216b9ee40cf320fbaa7a1c2593c
Kyle Poore <kylepoore1@gmail.com>
2014-04-27 02:57:34 +0000

    add ships functionality

---
+++ src/helpers.h
@@ -0,0 +1,17 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+typedef enum cell_type{
+  WATER,
+  SHIP,
+  FIRE
+} cell_type;
+
+typedef enum guess_type{
+  NOTHING,
+  MISS,
+  HIT
+} guess_type;
+
+
+#endif //HELPERS_H
